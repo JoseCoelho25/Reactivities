3c7115b6b7122aaa08505d6e5995b51ece9d9154
JoseCoelho25 <zenani87@hotmail.com>
2023-04-19 17:46:41 +0100

    created details activity

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Grid, List } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 import { Activity } from '../../../app/models/activity';
 import ActivityList from './ActivityList';
+import ActivityDetails from '../details/ActivityDetails';
 
 interface Props {
     activities: Activity[];
@@ -11,7 +12,12 @@ export default function ActivityDashboard({activities}: Props) {
   return (
     <Grid>
         <Grid.Column width='10'>
-        <ActivityList activities={activities} />
+            <ActivityList activities={activities} />
+        </Grid.Column>
+        <Grid.Column width='6'>
+            {activities[0] &&
+                <ActivityDetails activity={activities[0]} />
+            }
         </Grid.Column>
     </Grid>
   )
