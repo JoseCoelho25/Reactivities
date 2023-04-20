bdf2b7cd8cbed0545330edab26b8d53ac06e2dc8
JoseCoelho25 <zenani87@hotmail.com>
2023-04-20 20:47:14 +0100

    setup mobx and store context

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,12 @@
+import { makeObservable, observable } from 'mobx';
+
+export default class activityStore {
+    title = 'Hello from Mobx'
+
+    constructor() {
+        makeObservable(this, {
+            title: observable
+        });
+        
+    }
+}
