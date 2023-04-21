5c9cd90819ae41745058e6c91bdcbb7a97fadabc
JoseCoelho25 <zenani87@hotmail.com>
2023-04-21 17:35:47 +0100

    styling list items

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -16,13 +16,9 @@ return (
                 <Header sub color='teal'>
                     {group}
                 </Header>
-                <Segment>
-                    <Item.Group divided>
                         {activities.map(activity => (
                             <ActivityListItem  key={activity.id} activity={activity} />
                         ))}
-                </Item.Group>
-                </Segment>
             </Fragment>
         ))}
     </>
