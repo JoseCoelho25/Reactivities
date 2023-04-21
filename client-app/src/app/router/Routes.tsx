f5475248cde33edb86f2ee9af86ac3839f09e1fe
JoseCoelho25 <zenani87@hotmail.com>
2023-04-21 17:00:49 +0100

    code cleanup and added cancel btn funct

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -2,7 +2,6 @@ import { RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../layout/App";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
-import HomePage from "../../features/home/HomePage";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 
 export const routes: RouteObject[] = [
