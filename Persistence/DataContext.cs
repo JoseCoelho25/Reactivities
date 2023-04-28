4a953a72ac268f6c644abd94d459129f35c3c9e0
JoseCoelho25 <zenani87@hotmail.com>
2023-04-28 21:05:49 +0100

    configuring relations between the 2 initial tables

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,6 +12,24 @@ namespace Persistence
 
         public DbSet <Activity> Activities { get; set; }  //This creates the table name
         
-        
+        public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ActivityAttendee>(x=> x.HasKey(aa=> new {aa.AppUserId, aa.ActivityId})); //this will form the primary key
+
+            //configure the entity itself
+            builder.Entity<ActivityAttendee>()
+                .HasOne(u=> u.AppUser)
+                .WithMany(a => a.Activities)
+                .HasForeignKey(a=>a.AppUserId);
+
+                builder.Entity<ActivityAttendee>()
+                .HasOne(u=> u.Activity)
+                .WithMany(a => a.Attendees)
+                .HasForeignKey(a=>a.ActivityId);
+        }
     }
 }
